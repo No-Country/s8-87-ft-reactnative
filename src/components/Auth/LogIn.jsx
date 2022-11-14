46c2b6b87cfbe34735d53685eda04599cd1cff99
PabloMesquida <pablomesquida67@gmail.com>
2022-11-14 23:53:11 +0100

    test

--- src/components/Auth/LogIn.jsx
+++ src/components/Auth/LogIn.jsx
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 export default function LogIn() {
   return (
     <View>
-      <Text>LogIn</Text>
+      <Text>LogIn Log</Text>
     </View>
-  )
+  );
 }
