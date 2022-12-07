bdd0b03810a4a7b73261b3283cbcec13ab640785
Juanrubenc <rubencnino2@gmail.com>
2022-12-07 00:46:07 -0600

    Casting Card Added

--- src/screens/Castings.jsx
+++ src/screens/Castings.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import CastingCard from '../components/Castings/CastingCard'
 
 export default function Castings() {
   return (
     <View>
-      <Text>Castings</Text>
+      <CastingCard/>
     </View>
   )
 }
